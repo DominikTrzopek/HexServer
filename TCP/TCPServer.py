19f9ddcdae2e06762d36c6dcda5fe004ca2930c8
b-27 Ubuntu <vuko.trz@gmail.com>
2022-11-11 23:08:55 +0100

    Adds multiple TCP client handling

--- TCP/TCPServer.py
+++ TCP/TCPServer.py
@@ -2,6 +2,7 @@ import imp
 import json
 import socket
 import os
+import threading
 from Mongo.DBHandler import DBHandler
 from Mongo.DBHandler import Config
 
@@ -15,8 +16,41 @@ class TCPServer():
         self.ports = ports
         self.database = DBHandler()
         self.sockets = self.prepare_sockets()
-        self.connections = 1
-        self.listen(self.sockets[0])
+        self.connections = 0
+        #self.authorisefffff(self.sockets[0])
+        self.connected_ports = self.connect_clients(self.sockets)
+
+    def connect_clients(self, sockets):
+        for socket in sockets:
+            thread = threading.Thread(target = self.listen_for_connections, args = (socket,))
+            thread.start()
+
+    def listen_for_connections(self, socket):
+        socket.listen()
+        conn, addr = socket.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            data = conn.recv(bufferSize)
+            print(data)
+            check = self.authorise(data)
+            if check:
+                self.update_num_of_connections(1)
+            conn.sendall(data)
+
+    def authorise(self, data):
+        message = json.loads(str(data, 'utf-8'))
+        if message["password"] == self.password:
+            print("Correct password")
+            return True
+        return False
+
+    def update_num_of_connections(self, val):
+        self.connections += val
+        self.database.modify_data(
+            Config.get_server_collection(),
+            {'pid': os.getpid()},
+            {"$set": {'connections': self.connections}} #TODO set value
+        )
 
     def prepare_sockets(self):
         sockets = []
@@ -26,7 +60,7 @@ class TCPServer():
             sockets.append(sock)
         return sockets
 
-    def listen(self, socket):
+    def authorisefffff(self, socket):
         socket.listen()
         conn, addr = socket.accept()
         self.database.modify_data(
@@ -38,8 +72,6 @@ class TCPServer():
             self.connections += 1
             print(f"Connected by {addr}")
             while True:
-                data = conn.recv(bufferSize)
-                print(data)
-                if not data:
-                    break
-                conn.sendall(data)
+               # data = conn.recv(bufferSize)
+               # print(data)
+                conn.sendall(str.encode("Server says hi"))
